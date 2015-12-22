81385b87babd537f8c4180b08766ee65e123b483
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2015-12-22 17:10:28 +0200

    Lisätty count, joka laskee "animal" tagit.

--- xml/xml/Program.cs
+++ xml/xml/Program.cs
@@ -17,7 +17,17 @@ namespace xml
             Console.WriteLine(now);
 
             XmlDocument doc = new XmlDocument();
-            doc.Load("c:\\temp\\bigxml.xml");
+            doc.Load(@"c:\temp\bigxml.xml");
+
+            // Get and display all the animals titles.
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList elemList = root.GetElementsByTagName("animal");
+            Console.WriteLine("kpl " + elemList.Count);
+            /*for (int i = 0; i < elemList.Count; i++)
+            {
+                Console.WriteLine(elemList[i].InnerXml);
+            }*/
+
             now = DateTime.Now;
             Console.WriteLine(now);
             Console.WriteLine("Tiedosto luettu.");
